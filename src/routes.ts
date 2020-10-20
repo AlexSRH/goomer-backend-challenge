0cf3b34a717aad0da4b5a65fb3ec0d505ab46735
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-19 22:57:41 -0300

    Add routes to DELETE and PATCH

--- src/routes.ts
+++ src/routes.ts
@@ -9,5 +9,7 @@ const upload = multer(uploadConfig);
 routes.get('/restaurants', RestaurantsController.index);
 routes.get('/restaurants/:id', RestaurantsController.show);
 routes.post('/restaurants', upload.array('images'), RestaurantsController.create);
+routes.patch('/restaurants/:id', upload.array('images'), RestaurantsController.update);
+routes.delete('/restaurants/:id', RestaurantsController.delete);
 
 export default routes;
