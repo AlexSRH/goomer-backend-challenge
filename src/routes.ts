ec9d7c3c31170e75f05acb3d14de814662371b72
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-20 21:35:48 -0300

    Organize the code

--- src/routes.ts
+++ src/routes.ts
@@ -8,8 +8,11 @@ const upload = multer(uploadConfig);
 
 routes.get('/restaurants', RestaurantsController.index);
 routes.get('/restaurants/:id', RestaurantsController.show);
+
 routes.post('/restaurants', upload.array('images'), RestaurantsController.create);
+
 routes.patch('/restaurants/:id', upload.array('images'), RestaurantsController.update);
+
 routes.delete('/restaurants/:id', RestaurantsController.delete);
 
 export default routes;
