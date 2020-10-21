f587ad1145ec2d0945225326a2509a19c204e8d3
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-20 21:35:24 -0300

    Add Create Products migrations

---
+++ src/database/migrations/1603232408759-create-products.ts
@@ -0,0 +1,48 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createProducts1603232408759 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            unsigned: true,
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+            scale: 10,
+            precision: 2,
+          },
+          {
+            name: 'longitude',
+            type: 'decimal',
+            scale: 10,
+            precision: 2,
+          },
+          {
+            name: 'about',
+            type: 'text',
+          },
+          {
+            name: 'opening_hours',
+            type: 'varchar',
+          }
+        ],
+      }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
