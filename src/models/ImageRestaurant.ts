7e44005f3a67af3e027b022e024b3ae91e8a3511
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-21 23:08:21 -0300

    Add Image Restaurant Model

---
+++ src/models/ImageRestaurant.ts
@@ -0,0 +1,15 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
+import Restaurant from './Restaurant';
+
+@Entity('imagesProduct')
+export default class Image {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  path: string;
+
+  @ManyToOne(() => Restaurant, restaurant => restaurant.images)
+  @JoinColumn({ name: 'restaurant_id' })
+  restaurant: Restaurant;
+}
